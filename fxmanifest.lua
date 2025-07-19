450b7847e0829b99d42739b9b427e2abbd22a84f
Loreose <yyagizozcicekk@gmail.com>
2025-07-19 20:57:05 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Loreose'
+description 'ROSE Black Contact'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
